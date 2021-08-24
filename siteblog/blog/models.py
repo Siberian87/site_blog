95b7e6702abd22ffed09feabccfd2b2686752135
Siberian87 <siberiancoldman87@gmail.com>
2021-08-25 00:49:07 +0300

    Кастомизировал админку, добавил новые шаблоны, обновил модели

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.fields import CharField
+from django.urls import reverse
 
 '''
 Category
@@ -20,10 +20,18 @@ class Category(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
 
+
     def __str__(self):
         return self.title
 
+
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"slug": self.slug})
+    
+
     class Meta:
+        verbose_name = "Категория(ю)"
+        verbose_name_plural = "Категории"
         ordering = ['title']
 
 
@@ -36,6 +44,8 @@ class Tag(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
         ordering = ['title']
 
 
@@ -56,4 +66,6 @@ class Post(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = "Статья(ю)"
+        verbose_name_plural = "Статьи"
         ordering = ['-created_at']
