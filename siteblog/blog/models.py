b3ac692c216505809fa69907f6d1b6e63885a100
Siberian87 <siberiancoldman87@gmail.com>
2021-08-26 12:20:34 +0300

    Добавил пагинацию на страницу

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -40,8 +40,10 @@ class Tag(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
 
+
     def __str__(self):
         return self.title
+
 
     class Meta:
         verbose_name = "Тег"
@@ -62,8 +64,14 @@ class Post(models.Model):
         related_name='posts')
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
 
+
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return reverse("post", kwargs={"slug": self.slug})    
+
 
     class Meta:
         verbose_name = "Статья(ю)"
